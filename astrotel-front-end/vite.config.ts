8e522fd7622de90e4cc2ff191514497e609bb034
Nishtha <nishtha@admins-MacBook-Air.local>
2024-04-28 17:29:00 +0530

    empty commit

--- astrotel-front-end/vite.config.ts
+++ astrotel-front-end/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react (), nodePolyfills()],
   base: "/astrotel-front-end/",
