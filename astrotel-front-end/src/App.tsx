893b856d348f1babc60fe2ea0cd200b700d41ed9
Nishtha <nishtha@admins-MacBook-Air.local>
2024-05-07 14:24:39 +0530

    TWA SDK integrated

--- astrotel-front-end/src/App.tsx
+++ astrotel-front-end/src/App.tsx
@@ -1,9 +1,11 @@
+import WebApp from '@twa-dev/sdk';
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
 
 function App() {
   return (
     <div>
+      <b>{WebApp. platform}</b>
       <TonConnectButton />
     </div>
   );
