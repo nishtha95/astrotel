45520f80862cf511f4c8f41e62eef3f41b235d55
Nishtha <nishtha@admins-MacBook-Air.local>
2024-05-07 17:25:04 +0530

    bot created

---
+++ astrotel-tg-bot/src/index.ts
@@ -0,0 +1,21 @@
+import { Telegraf } from "telegraf";
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.TG_BOT_TOKEN);
+const bot = new Telegraf(process.env.TG_BOT_TOKEN || "");
+// bot.start((ctx) =>
+//   ctx.reply("Welcome to our counter app!", {
+//     reply_markup: {
+//       keyboard: [
+//         ["Increment by 5"],
+//         ["Deposit 1 TON"],
+//         ["Withdraw 0.7 TON"],
+//       ],
+//     },
+//   })
+// );
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop ("SIGTERM"));
