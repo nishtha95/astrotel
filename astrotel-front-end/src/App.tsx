43d03afdca16f12e87c893120505b3caa54c1c7b
Nishtha <nishtha@admins-MacBook-Air.local>
2024-05-09 14:10:53 +0530

    tailwind and konsta ui added

--- astrotel-front-end/src/App.tsx
+++ astrotel-front-end/src/App.tsx
@@ -1,12 +1,16 @@
 import WebApp from '@twa-dev/sdk';
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
+import { Block, Button } from 'konsta/react';
 
 function App() {
   return (
-    <div>
-      <b>{WebApp. platform}</b>
-      <TonConnectButton />
+    <div >
+      <div className='flex justify-center'><TonConnectButton/></div>
+      <div className='m-5'/>
+      <Button>Log in as Astrologer</Button>
+      <div className='m-5'/>
+      <Button>Log in as Client</Button>
     </div>
   );
 }
---
+++ astrotel-front-end/tailwind.config.js
@@ -0,0 +1,12 @@
+const konstaConfig = require('konsta/config');
+/** @type {import('tailwindcss').Config} */
+export default konstaConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
