c963892b21ac1a7672859f75db23eafaf6fd4c58
Nishtha <nishtha@admins-MacBook-Air.local>
2024-06-20 19:55:23 +0530

    fix

--- astrotel-tg-bot/src/index.js
+++ astrotel-tg-bot/src/index.js
@@ -1,4 +1,6 @@
-const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
+const express = require("express"); 
+const app = express(); 
+app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
 const PORT = process.env.PORT || 8000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 module.exports = app;
 
